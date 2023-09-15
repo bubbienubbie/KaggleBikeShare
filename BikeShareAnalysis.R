0334df606b141958d8830e1f67d8714f41503426
Isaac Dowdle <isaacdowdle@gmail.com>
2023-09-14 23:47:51 -0600

    First commit

---
+++ BikeShareAnalysis.R
@@ -0,0 +1,26 @@
+bike <- vroom("C:/Users/isaac/KaggleBikeShare/train.csv")
+
+myCleanData <- bike %>%
+filter(bike$weather !=4) %>% 
+  select(season, weather, temp, windspeed, count)
+
+my_recipe <- recipe(rFormula, data=myDataSet) %>% # Set model formula and d2
+  step_mutate(newVar=var1*var2) %>% #Create a new variable3
+  step_poly(var, degree=2) %>% #Create polynomial expansion of var4
+  step_date(timestamp, features="dow") %>% # gets day of week5
+  step_time(timestamp, features=c("hour", "minute")) %>%6
+step_dummy(all_nominal_predictors()) %>% #create dummy variables7
+  step_zv(all_predictors()) %>% #removes zero-variance predictors8
+  step_corr(variables, threshold=0.5) %>% # removes > than .5 corr9
+  step_rm(var) %>% #removes a variables10
+  step_select(var, -var2) #selects columns11
+prepped_recipe <- prep(my_recipe) # Sets up the preprocessing using myDataS12
+bake(prepped_recipe, new_data=AnotherDataSet)
+
+
+my_recipe <- recipe(count ~ ., data=myCleanData) %>%
+  step_num2factor(season, levels=c("spring","summer","fall","winter"))
+  step_mutate(newVar=log(windspeed))
+prepped_recipe <- prep(my_recipe)
+bake(prepped_recipe, new_data=myCleanData)
+
---
+++ BikeShareEDA.R
@@ -0,0 +1,29 @@
+install.packages("tidyverse")
+install.packages("DataExplorer")
+install.packages("vroom")
+install.packages("GGally")
+
+library(tidyverse)
+library(vroom)
+library(DataExplorer)
+library(GGally)
+
+bike <- vroom("C:/Users/isaac/KaggleBikeShare/train.csv")
+
+dplyr::glimpse(bike)
+DataExplorer::plot_intro(bike)
+DataExplorer::plot_correlation(bike)
+DataExplorer::plot_bar(bike)
+DataExplorer::plot_histrograms(bike)
+DataExplorer::plot_missing(bike) 
+GGally::ggpairs(bike)
+
+plot1 <- ggplot(data=bike, aes(x=temp, y=count)) + geom_point() + geom_smooth(se=FALSE)
+plot2 <- ggplot(data=bike, aes(x=atemp, y=count)) + geom_point() + geom_smooth(se=FALSE)
+plot3 <- ggplot(data=bike, aes(x=windspeed, y=count)) + geom_point() + geom_smooth(se=FALSE)
+plot4 <- ggplot(data=bike, aes(x=humidity, y=count)) + geom_point() + geom_smooth(se=FALSE)
+(plot1 + plot2) / (plot3 + plot4)
+
+
+install.packages("tidymodels")
+library(tidymodels)
